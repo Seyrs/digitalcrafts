7b8814f35c5b713ebd31c16606a96d537bdaa4da
SeymouR Scott <seymourscott@Dark-Romantic-Apples.local>
2019-09-12 19:37:02 -0400

    making first formula

---
+++ 9_12_2019_class_notes/formula1.js
@@ -0,0 +1,30 @@
+length  = 10;
+
+width = 15;
+
+area = length * width;
+
+x = 10;
+y  = 15;
+z = x * y;
+
+// The formula for the LTV - Lifetime Value of a customer in business
+// LTV = (ARPA * Gross margin)//CCR
+// ARPA: Average Revenue per account (the average MRR across all of your active customers)
+// Gross Margin: the difference between revenue and COGS (cost of goods sold). This is typically extremely high in Sales
+// Customer Churn Rate: The rate at which your customers are cancellin their subscriptions.)
+
+
+// var length = 10;
+// var width = 5'
+//
+
+// var area = width * length;
+// console.log(area);
+
+
+var arpa = 5;
+var grossMargin = 8;
+var ltv = arpa * grossMargin;
+console.log(ltv);
+ 
