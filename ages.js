b8297beca027b5be82a53009060964121338e06c
SeymouR Scott <seymourscott@Dark-Romantic-Apples.local>
2019-09-14 13:17:20 -0400

    age functions failed

---
+++ ages.js
@@ -0,0 +1,57 @@
+//**** attempt 1*****
+//var s = true;
+//var b = true;
+//var h = true;
+//var k = false;
+//age = 16;
+//fucntion canDrive() {
+//if ( >= age) {
+//	return true;
+//}
+//else {
+//	return false;
+//}
+//}
+//console.log (if 1, "Sarah is old);
+
+
+
+
+
+
+
+//********** ATTEMPT 2****************	
+
+var sarah = function(age) {
+	if (age>=16) {
+	return true;
+}	else {
+	return false;
+};
+if (sarah (age) == 1) {
+	console.log("Sarah is old enough to drive.");
+}	else{
+	console.log("Sarah isn't old enough to drive.");
+}
+//********fail again*********
+
+
+
+
+
+// Above and Below Incomplete
+
+
+function drink() {
+
+}
+
+function rent() {
+
+}
+
+function play() {
+
+}
+
+
